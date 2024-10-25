4c1279c72f5619ed391ae2417d4f882daf384348
Nikokahila <nkahila12@gmail.com>
2024-10-25 10:55:40 +0300

    Lisäsin lucky 7 tehtävän

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Harjoittelen Gitin käyttöä");
+        System.out.println("Tein lucky7 tehtävän");
+
+        Random random = new Random();
+        
+        // Arvotaan kolme numeroa välillä 1-10
+        int numero1 = random.nextInt(10) + 1;
+        int numero2 = random.nextInt(10) + 1;
+        int numero3 = random.nextInt(10) + 1;
+        
+        
+        System.out.println("Arvotut numerot ovat: " + numero1 + ", " + numero2 + ", " + numero3);
+        
+        
+        if (numero1 == 7 || numero2 == 7 || numero3 == 7) {
+            System.out.println("Voitit!");
+        } else {
+            System.out.println("Hävisit.");
+        }
     }
 }
+
+
+    
